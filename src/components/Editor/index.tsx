566d555593ded494db2651f93e34e8158fbe322c
Rishabh Mishra <me@rsbh.dev>
2023-02-12 22:40:16 +0530

    setup react-dnd

---
+++ src/components/Editor/index.tsx
@@ -0,0 +1,69 @@
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { useState } from "react";
+
+export const ItemTypes = {
+  Button: "button",
+};
+
+const Dragable = (props: any) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: ItemTypes.Button,
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+    item: {
+      id: props.id,
+    },
+  }));
+  return (
+    <div
+      className="component-small"
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: 25,
+        fontWeight: "bold",
+        cursor: "move",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+const Canvas = (props: any) => {
+  const [collectedProps, drop] = useDrop(() => ({
+    accept: ItemTypes.Button,
+    drop: (item) => {
+      props.onDrop(item);
+    },
+  }));
+
+  return (
+    <div className="drawer" ref={drop}>
+      {props.componentList.map((c: any, i: number) => (
+        <div key={c.id + i}>{c.id}</div>
+      ))}
+    </div>
+  );
+};
+
+export default function Editor() {
+  const [componentList, setComponentList] = useState<any>([]);
+
+  function onDrop(item: any) {
+    setComponentList([...componentList, item]);
+  }
+
+  return (
+    <div className="editor">
+      <DndProvider backend={HTML5Backend}>
+        <div className="component-list">
+          <Dragable id={"btn-1"}>Button</Dragable>
+        </div>
+        <Canvas componentList={componentList} onDrop={onDrop}></Canvas>
+      </DndProvider>
+    </div>
+  );
+}

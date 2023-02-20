1affcaac3fa416aee786e5bab9214f9e5426ca9a
Rishabh Mishra <me@rsbh.dev>
2023-02-20 14:56:42 +0530

    chore: add WidgetStyleProperties

--- src/constants/widget.tsx
+++ src/constants/widget.tsx
@@ -1,29 +1,49 @@
-import { WidgetItem } from "../types/widget";
+import { BaseWidget, WidgetTypes } from "../types/widget";
 export const DefaultDragType = "Widget";
 
-export const widgetList: WidgetItem[] = [
-  {
+export const BASE_WIDGET_MAP: Record<WidgetTypes, BaseWidget> = {
+  BUTTON: {
     title: "Button",
     type: "BUTTON",
     previewStyle: {
       height: "32px",
       width: "96px",
     },
+    styleProperties: [
+      { id: "height", label: "Height", default: "32px", type: "string" },
+      { id: "width", label: "Width", default: "96px", type: "string" },
+      { id: "left", label: "Left", default: "0px", type: "string" },
+      { id: "top", label: "Top", default: "0px", type: "string" },
+    ],
   },
-  {
+  INPUT: {
     title: "Input",
     type: "INPUT",
     previewStyle: {
       height: "32px",
       width: "200px",
     },
+    styleProperties: [
+      { id: "height", label: "Height", default: "32px", type: "string" },
+      { id: "width", label: "Width", default: "200px", type: "string" },
+      { id: "left", label: "Left", default: "0px", type: "string" },
+      { id: "top", label: "Top", default: "0px", type: "string" },
+    ],
   },
-  {
+  TABLE: {
     title: "Table",
     type: "TABLE",
     previewStyle: {
       height: "400px",
       width: "304px",
     },
+    styleProperties: [
+      { id: "height", label: "Height", default: "400", type: "string" },
+      { id: "width", label: "Width", default: "304px", type: "string" },
+      { id: "left", label: "Left", default: "0px", type: "string" },
+      { id: "top", label: "Top", default: "0px", type: "string" },
+    ],
   },
-];
+};
+
+export const widgetList: BaseWidget[] = Object.values(BASE_WIDGET_MAP);
--- src/types/widget.tsx
+++ src/types/widget.tsx
@@ -7,10 +7,18 @@ export const WidgetsTypeMap = {
   TABLE: "TABLE",
 } as const;
 
+export interface WidgetStyleProperties {
+  id: keyof CSSProperties;
+  label: string;
+  type: string;
+  default: any;
+}
+
 export interface BaseWidget {
   title: string;
   type: WidgetTypes;
   previewStyle: CSSProperties;
+  styleProperties: WidgetStyleProperties[];
 }
 
 export type WidgetTypes = keyof typeof WidgetsTypeMap;
@@ -19,6 +27,8 @@ export interface DropItem {
   baseWidget: BaseWidget;
   isNewWidget: boolean;
   id: string;
+  widgetType: WidgetTypes;
+  styles: CSSProperties;
 }
 
 export interface WidgetComponent extends DropItem {

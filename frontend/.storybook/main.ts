210ec3fa0437e2cfd06a42e85132f7a9ac6ec945
Rishabh Mishra <me@rsbh.dev>
2023-03-01 11:44:10 +0530

    chore: setup storybook

---
+++ frontend/.storybook/main.ts
@@ -0,0 +1,21 @@
+import { mergeConfig } from "vite";
+export default {
+  stories: ["../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Add storybook-specific dependencies to pre-optimization
+      optimizeDeps: {
+        include: ["storybook-addon-designs"],
+      },
+    });
+  },
+  docs: {
+    autodocs: true,
+  },
+};

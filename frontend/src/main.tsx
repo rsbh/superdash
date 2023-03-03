5a871de9a5506f22602b037c2853d990b6075158
Rishabh Mishra <me@rsbh.dev>
2023-03-03 12:42:11 +0530

    chore: add theme provider to the app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import theme from "./config/theme";
 import "./index.css";
 import Router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Router />
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
   </React.StrictMode>
 );
